e8dc7b33be074a784ea893b7ef6988ea59025182
ABHINAVRAJ T <abhinavrajt2004@gmail.com>
2025-08-03 10:39:00 +0530

    boomboom

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE ANON KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
